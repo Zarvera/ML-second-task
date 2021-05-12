d32b94388e8534cfa060bfe68bac855f295e6ef6
Zarvera <hexagent@gmail.com>
2021-05-13 01:02:13 +0300

    Добавил form и filter datepicker, поправил филдсет

---
+++ src/blocks/date-filter/date-filter.js
@@ -0,0 +1,8 @@
+import '../../../node_modules/air-datepicker/dist/js/datepicker';
+
+$('.date-filter').datepicker({
+    dateFormat: 'd M',
+    language: {
+        monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек']
+    }
+});
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './style.scss';
 import './blocks/dropdown/dropdown';
 import './blocks/masked-input/masked-input';
 import './blocks/datepicker/datepicker';
+import './blocks/date-filter/date-filter';
