8ac295d1ac0717c7aabd292800dd0490221ee5c4
Zarvera <hexagent@gmail.com>
2021-05-13 01:03:57 +0300

    Подправил datepicker

--- src/blocks/datepicker/datepicker.js
+++ src/blocks/datepicker/datepicker.js
@@ -1,15 +1,15 @@
 import '../../../node_modules/air-datepicker/dist/js/datepicker';
 
-$('.my-datepicker, .datepicker-from').datepicker({ 
+$('.datepicker__field_from').datepicker({ 
     onSelect: function (fd, d, picker) { 
-        $(".my-datepicker, .datepicker-from").val(fd.split("-")[0]);
-        $(".datepicker-to").val(fd.split("-")[1]);
+        $(".datepicker__field_from").val(fd.split("-")[0]);
+        $(".datepicker__field_to").val(fd.split("-")[1]);
     }
 });
 
-$('.my-datepicker, .datepicker-to').datepicker({ 
+$('.datepicker__field_to').datepicker({ 
     onSelect: function (fd, d, picker) { 
-        $(".my-datepicker, .datepicker-to").val(fd.split("-")[1]);
-        $(".datepicker-from").val(fd.split("-")[0]);
+        $(".datepicker__field_to").val(fd.split("-")[1]);
+        $(".datepicker__field_from").val(fd.split("-")[0]);
     }
 });
