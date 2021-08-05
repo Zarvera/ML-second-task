23edaf56397a4b7019d8d0ee68f41244796fa086
Zarvera <hexagent@gmail.com>
2021-08-05 03:23:15 +0300

    Подправил range-slider

--- src/blocks/range-slider/range-slider.js
+++ src/blocks/range-slider/range-slider.js
@@ -1,13 +1,23 @@
 import noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 
-var slider = document.getElementById('range-slider');
+var nonLinearStepSlider = document.getElementById('range-slider');
+var nonLinearStepSliderValueElement = document.getElementById('slider-non-linear-step-value');
 
-noUiSlider.create(slider, {
-    start: [20, 80],
+noUiSlider.create(nonLinearStepSlider, {
+    start: [5000, 10000],
     connect: true,
     range: {
-        'min': 0,
-        'max': 100
+        'min': 1000,
+        'max': 15000
+    },
+    step: 100,
+    format: {
+      to: (v) => parseFloat(v).toFixed(0) + '₽',
+      from: (v) => parseFloat(v).toFixed(0)
     }
+});
+
+nonLinearStepSlider.noUiSlider.on('update', function (values) {
+    nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
 });
