0348cca97ee34d3f04b827f7d97527613a9cfb67
Zarvera <hexagent@gmail.com>
2021-08-07 02:50:41 +0300

    Частично перевёл form elements в отдельную страниц

--- src/blocks/text-input/text-input.js
+++
@@ -1,5 +0,0 @@
-import '../../vendors/js/jquery.inputmask.min.js';
-
-$(document).ready(function() {
-    $('.text-input_masked').inputmask('99.99.9999', {"placeholder": "ДД.ММ.ГГГГ"}); 
-});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './main.scss';
+import 'jquery-ui-bundle';
+import './vendors/js/jquery.inputmask.min.js';
+import './blocks/dropdown/dropdown';
+import './blocks/datepicker/datepicker';
+import './blocks/date-filter/date-filter';
+import './blocks/like-button/like-button';
+import './blocks/rate-button/rate-button';
+import './blocks/range-slider/range-slider';
+import './blocks/pagination/paginations';
+import './blocks/expandable/expandable';
+
+(function() {
+  $('#mask').inputmask('99.99.9999', {"placeholder": "ДД.ММ.ГГГГ"});
+  $('#like-inited').trigger('click'),
+  $('#rate-inited4').children('[for="rate4"]').trigger('click'),
+  $('#rate-inited5').children('[for="rate5"]').trigger('click'),
+  $('#dd1').dropdownstepper(),
+  $('#dd2').dropdownstepper( {
+    pronounces: [['спальня', 'спальни', 'спален'], ['кровать', 'кровати', 'кроватей'], ['ванная комната', 'ванные комнаты', 'ванных комнат']],
+    declension: true
+  }),
+  $('#dd3').dropdownstepper( {
+    pronounces: [['спальня', 'спальни', 'спален'], ['кровать', 'кровати', 'кроватей'], ['ванная комната', 'ванные комнаты', 'ванных комнат']],
+    declension: true
+  }),
+  $('#dd3').trigger('click'),
+  $('#dd3').siblings('.dropdown__menu-option_open').find('.button_increment').eq(0).trigger('click').trigger('click'),
+  $('#dd3').siblings('.dropdown__menu-option_open').find('.button_increment').eq(1).trigger('click').trigger('click'),
+  $('#init-text_input').addClass('text-input_hovered').val('This is pretty awesome')
+})();
