9f8fe555c4465e071b5898cacb94ea73115edfa9
Zarvera <hexagent@gmail.com>
2021-08-24 01:10:42 +0300

    Начал лендинг\переход на тестовую ветку

--- webpack.common.js
+++ webpack.common.js
@@ -23,6 +23,7 @@ module.exports = {
     'colors-and-type': `${PATHS.src}/assets/js/colors-and-type.js`,
     'cards': `${PATHS.src}/assets/js/cards.js`,
     'header-and-footer': `${PATHS.src}/assets/js/header-and-footer.js`,
+    'landing-page': `${PATHS.src}/assets/js/landing-page.js`,
   },
   output: {
     filename: `${PATHS.assets}js/[name].js`,
