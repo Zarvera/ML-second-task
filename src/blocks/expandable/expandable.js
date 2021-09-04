22162fb8f3d1357b9d22ed00edcca60ad3e534aa
Zarvera <hexagent@gmail.com>
2021-09-04 22:11:03 +0300

    expandable сворачивался при нажатии на чекбокс

--- src/blocks/expandable/expandable.js
+++ src/blocks/expandable/expandable.js
@@ -1,10 +1,10 @@
-$('.expandable').on('click', function() {
-  if($(this).find('.expandable__content').is(':hidden')) {
+$('.expandable__fieldset-wrap').on('click', function() {
+  if($(this).siblings(".expandable__content").is(':hidden')) {
     $(this).find('.expandable__expand-icon').css('transition', 'transform 0.5s');
     $(this).find('.expandable__expand-icon').css('transform', 'rotate(0.5turn)');
-    $(this).find('.expandable__content').slideDown('fast');
+    $(this).siblings(".expandable__content").slideDown('fast');
   } else {
-    $(this).find('.expandable__content').slideUp('fast');
+    $(this).siblings(".expandable__content").slideUp('fast');
     $(this).find('.expandable__expand-icon').css('transform', 'rotate(0turn)');
   }
 })
