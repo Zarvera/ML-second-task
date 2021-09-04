75200797d0f3c105fb8a82ffc8886bdea7b0afb8
Zarvera <hexagent@gmail.com>
2021-09-04 22:51:54 +0300

    Доделал search room

--- src/assets/js/search-room.js
+++ src/assets/js/search-room.js
@@ -5,3 +5,9 @@ import '../../blocks/date-filter/date-filter';
 import '../../blocks/range-slider/range-slider';
 import '../../blocks/pagination/paginations';
 import '../../blocks/expandable/expandable';
+
+$("#dd1").dropdownstepper({buttons: true});
+$('#dd2').dropdownstepper( {
+  pronounces: [['спальня', 'спальни', 'спален'], ['кровать', 'кровати', 'кроватей'], ['ванная комната', 'ванные комнаты', 'ванных комнат']],
+  declension: true
+});
