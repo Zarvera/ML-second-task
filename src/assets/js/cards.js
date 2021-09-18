fb4b673dbed15c3b3cb9ba404f5a7540493e5ee0
Zarvera <hexagent@gmail.com>
2021-09-18 22:30:53 +0300

    Прицепил js booking к карточкам

--- src/assets/js/cards.js
+++ src/assets/js/cards.js
@@ -3,6 +3,7 @@ import '../../templates/cards.scss';
 import '../../blocks/dropdown/dropdown';
 import '../../blocks/datepicker/datepicker';
 import '../../blocks/lookup/lookup';
+import '../../blocks/booking/booking';
 
 (function() {
   $('.datepicker_inline').datepicker({
